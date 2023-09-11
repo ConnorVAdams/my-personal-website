41ba734d13e5bf9e8bee70f491da91675ec29e96
Connor Adams <connorvadams@gmail.com>
2023-09-11 07:41:25 -0600

    upload first draft of website

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript.";
+function show() {
+    let onOrOff = document.getElementById('info-content');
+    if (onOrOff.style.visibility = 'hidden') {
+            onOrOff.style.visibility = 'visible'
+        }
+}
 
-document.querySelector("body").appendChild(h2);
+function hide() {
+    let onOrOff = document.getElementById('info-content');
+    if (onOrOff.style.visibility = 'visible') {
+        onOrOff.style.visibility = 'hidden'
+    }
+}
+
+const Badge = document.querySelectorAll('.badge').forEach(badgeButton => {
+    badgeButton.addEventListener('click', () => show());
+});
+
+const exitX = document.querySelectorAll('.exit-button').forEach(exitButton => {
+    exitButton.addEventListener('click', () => hide());
+});
